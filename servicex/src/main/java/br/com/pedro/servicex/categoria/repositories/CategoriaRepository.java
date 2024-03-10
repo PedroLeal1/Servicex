06861606814bdf7da0e125a31e7d08e0c4e0613c
Pedro Vieira <65934259+PedroLeal1@users.noreply.github.com>
2024-03-10 19:45:19 -0300

    Add files via upload

---
+++ servicex/src/main/java/br/com/pedro/servicex/categoria/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.pedro.servicex.categoria.repositories;
+
+import br.com.pedro.servicex.categoria.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
